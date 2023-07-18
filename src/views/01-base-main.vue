ae4866cf81d51bce52eca57b3b44c468f4c9bd24
githubUser1104 <qmail1474369@qq.com>
2023-07-18 17:05:33 +0800

    up

---
+++ src/views/01-base-main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>base</div>
+</template>
+
+<script>
+export default {
+  name: 'base-main',
+  // 子组件
+  components: {},
+  // 数据绑定
+  data () {
+    return {
+      text: 'text'
+    }
+  },
+  // 生命周期
+  created() {
+    console.log(this.text);
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
